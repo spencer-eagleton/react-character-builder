d21a3cf1ea4ca8670cf7e6d8b8b5ba7d2e3f097a
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-12-01 17:17:02 -0800

    created basic file tree

---
+++ src/views/Home/Home.js
@@ -0,0 +1,9 @@
+// import Picker from '../../components/Picker/Picker';
+// import Character from '../../components/Character/Character';
+
+import './Home.css';
+import background from '../../background.jpeg';
+
+export default function Home() {
+  return <main style={{ backgroundImage: `url(${background})` }}></main>;
+}
