86df8273a11b0be338cb94871169d0d9726daa97
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-12-03 15:32:08 -0800

    phrase styles

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -3,12 +3,11 @@ import './Character.css';
 export default function Character({ head, middle, pants, text, textList }) {
   return (
     <div className="character">
-      <span>{text}</span>
-
+      <span className="new">{text}</span>
       <img width="100px" src={process.env.PUBLIC_URL + `/assets/${head}.png`} />
       <img width="100px" src={process.env.PUBLIC_URL + `/assets/${middle}.png`} />
       <img width="100px" src={process.env.PUBLIC_URL + `/assets/${pants}.png`} />
-      <span className="phrases">{textList}</span>
+      <span className="phrases">{textList} </span>
     </div>
   );
 }
