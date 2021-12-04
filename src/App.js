38047b845c2cac37ae27ff5b044d59201aa773a3
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-12-03 16:17:05 -0800

    added header and footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 
 import Home from './views/Home/Home';
 import background from './background.jpeg';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="body" style={{ backgroundImage: `url(${background})` }}>
-      <Home />
-    </div>
+    <>
+      <div className="body" style={{ backgroundImage: `url(${background})` }}>
+        <Header />
+        <Home />
+        <Footer />
+      </div>
+      <footer />
+    </>
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,4 @@
+import './Footer.css';
+export default function Footer() {
+  return <footer>by Spencer Eagleton</footer>;
+}
